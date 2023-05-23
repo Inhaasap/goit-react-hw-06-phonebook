d1b7cd4186af56c4744a04df58865a776156aece
Інга Юрченко <missingusya@hotmail.com>
2023-05-23 15:04:22 +0300

    updated

---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 12px;
+
+  width: 400px;
+  max-width: 100%;
+
+  padding: 20px;
+  margin: 10px auto;
+
+  border-radius: 10px;
+  box-shadow: inset 1px 1px 3px #8ba793, inset -1px -1px 5px #fff;
+  background-color: rgb(212, 239, 224);
+`;
+
+export const Title = styled.h1`
+  text-align: center;
+  font-size: 30px;
+`;
+
+export const SubTitle = styled.h1`
+  text-align: center;
+  font-size: 30px;
+  margin-top: 10px;
+`;
---
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const FormLabel = styled.label`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 15px;
+  margin-bottom: 15px;
+`;
+
+export const LabelSpan = styled.span`
+  margin-right: 10px;
+  font-weight: 400;
+  font-size: 16px;
+`;
+
+export const BtnSubmit = styled.button`
+  cursor: pointer;
+  display: block;
+  margin: 0 auto;
+  margin-top: 20px;
+  padding: 10px 15px;
+  border: none;
+  outline: none;
+  text-transform: uppercase;
+  border-radius: 10px;
+  font-size: 15px;
+  letter-spacing: 0.02em;
+  font-weight: 500;
+  background-color: #7ca78c;
+  box-shadow: rgb(0 0 0 / 24%) 10px 10px 5px 0px;
+  transition: box-shadow 250ms ease-in-out, background-color 250ms ease-in-out;
+  
+  :hover,
+  :focus {
+    box-shadow: -2px -2px 5px #fff, 2px 2px 5px #8ba793;
+    background-color: #b1dfc1;
+  }
+`;
